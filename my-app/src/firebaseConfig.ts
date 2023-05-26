5ded24a36dd397011b04cab6cca047d9cfe57eef
KrakowiakP <krakowiakp96@gmail.com>
2023-05-26 14:18:39 +0200

    dodanie_firebase

---
+++ my-app/src/firebaseConfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCI4yKPm9DSh5rEpH77iVvSajJPuhfxv2U",
+  authDomain: "projekt-sda.firebaseapp.com",
+  projectId: "projekt-sda",
+  storageBucket: "projekt-sda.appspot.com",
+  messagingSenderId: "876912463103",
+  appId: "1:876912463103:web:19dfc027069c429303d0ca"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
