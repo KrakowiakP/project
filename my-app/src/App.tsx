05b01a390f025134a32ea9cd24cd0d8d94867d99
KrakowiakP <krakowiakp96@gmail.com>
2023-05-27 09:29:56 +0200

    dodanie home i button

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Route, Router, Routes } from "react-router-dom";
+import { Home } from "./Components/HOME";
 import { initializeApp } from "firebase/app";
 import { firebaseConfig } from "./firebaseConfig";
 
---
+++ my-app/src/Components/index.tsx
@@ -0,0 +1,11 @@
+import Button from "@mui/material/Button";
+
+export const Home = () => {
+	return (
+		<div>
+			<h2>HOMEPAGE</h2>
+			<Button variant="contained">Contained</Button>
+			<Button variant="outlined">Outlined</Button>
+		</div>
+	);
+};
